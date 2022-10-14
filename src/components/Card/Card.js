d8092f9e13a462f77239417f5764dd812c751222
Anthony Shellman <atshellman@gmail.com>
2022-10-13 20:04:36 -0600

    Add temp styling and minor code adjustments

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,9 +3,9 @@ import "./Card.css"
 
 const Card = ( {title, poster} ) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <img src={poster} alt={`Movie poster for ${title}`}></img>
+    <div className="card">
+      <img className="movie-poster" src={poster} alt={`Movie poster for ${title}`}></img>
+      <h2 className="movie-title">{title}</h2>
     </div>
   )
 }
--- src/components/MovieCardContainer/MovieCardContainer.js
+++ src/components/MovieCardContainer/MovieCardContainer.js
@@ -5,18 +5,15 @@ import Card from "../Card/Card"
 const MovieCardContainer = ( {movies}) => {
   const allMovies = movies.map(movie => {
     return (
-    <div>
     <Card
     title={movie.title}
     poster={movie.poster_path}
     />
-
-    </div>
     )
   })
 
   return (
-    <div>
+    <div className="movie-container">
       { allMovies }
     </div>
   )
