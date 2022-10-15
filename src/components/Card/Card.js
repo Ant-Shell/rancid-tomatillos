c9c63377eafaa62953662a2225c20b0ce3a45511
William Hobson <will.hobson85@gmail.com>
2022-10-14 21:20:46 -0400

    Add onClick functionality, adjusted indentation

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,9 @@
 import React from "react"
 import "./Card.css"
 
-const Card = ( {title, poster} ) => {
+const Card = ( {title, poster, showMovieDetails} ) => {
   return (
-    <div className="card">
+    <div onClick={() => showMovieDetails()} className="card">
       <img className="movie-poster" src={poster} alt={`Movie poster for ${title}`}></img>
       <h2 className="movie-title">{title}</h2>
     </div>
