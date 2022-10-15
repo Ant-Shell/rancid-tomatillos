74beba25e72f909393d5e71ae9b835d644c1a202
William Hobson <will.hobson85@gmail.com>
2022-10-15 12:09:01 -0400

    Update props

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,9 @@
 import React from "react"
 import "./Card.css"
 
-const Card = ( {title, poster, showMovieDetails} ) => {
+const Card = ( {title, poster, showMovieDetails, id} ) => {
   return (
-    <div onClick={() => showMovieDetails()} className="card">
+    <div onClick={() => showMovieDetails(id)} className="card">
       <img className="movie-poster" src={poster} alt={`Movie poster for ${title}`}></img>
       <h2 className="movie-title">{title}</h2>
     </div>
