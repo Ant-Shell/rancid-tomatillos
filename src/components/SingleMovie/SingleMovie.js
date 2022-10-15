d46f935a370a9c2863a644c2186ae5e0efbf88d1
William Hobson <will.hobson85@gmail.com>
2022-10-15 16:53:33 -0400

    Write CSS for individual movie view, adjust other CSS as needed.

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -3,12 +3,12 @@ import "./SingleMovie.css"
 
 const SingleMovie = (props) => {
     return (
-        <div>
+        <div className="movie-box">
             <h2>{props.singleMovie.title}</h2>
-            <div>
-                <h3>{props.singleMovie.average_rating}</h3>
-                <h3>{props.singleMovie.runtime}</h3>
-                <h3>{props.singleMovie.genre}</h3>
+            <div className="specifics">
+                <h3>Rating: {props.singleMovie.average_rating} </h3>
+                <h3>Runtime: {props.singleMovie.runtime} </h3>
+                <h3>Genre: {props.singleMovie.genres}</h3>
             </div>
             <h3>{props.singleMovie.overview}</h3>
             <button onClick={() => props.showAllMovies()}>View All</button>
