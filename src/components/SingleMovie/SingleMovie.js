951eabad779e9ec4bf4cc6fff96268d43236eeff
Anthony Shellman <atshellman@gmail.com>
2022-10-18 20:39:32 -0600

    Refactor as class component, import fetchSpecificDetails, add movie state

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -1,22 +1,40 @@
-import React from "react"
-import "./SingleMovie.css"
+import React, { Component } from "react"
+import { fetchSpecificDetails } from '../../apiCalls'
 import {Link} from "react-router-dom"
+import "./SingleMovie.css"
 
-const SingleMovie = ({movie}) => {
-    const {title, poster_path, average_rating, runtime, genres, overview} = movie
-    return (
-        <div className="movie-box">
-            <h2>{title}</h2>
-            <img className="movie-poster" src={poster_path} alt={`Movie poster for ${title}`}></img>
-            <div className="specifics">
-                <h3>Rating: {average_rating} </h3>
-                <h3>Runtime: {runtime} </h3>
-                <h3>Genre: {genres}</h3>
+class SingleMovie extends Component {
+    constructor() {
+        super()
+        this.state = {
+            movie: {}
+        }
+    }
+
+    componentDidMount = () => {
+        const id = this.props.id
+        fetchSpecificDetails(id)
+        .then(data => {
+         this.setState({movie: data.movie})})
+        .catch(err => console.error(err));
+      }
+
+    render = () => {
+        const singleMovie = this.state.movie
+        return (
+            <div className="movie-box">
+                <h2>{singleMovie.title}</h2>
+                <img className="movie-poster" src={singleMovie.poster_path} alt={`Movie poster for ${singleMovie.title}`}></img>
+                <div className="specifics">
+                <h3>Rating: {singleMovie.average_rating} </h3>
+                <h3>Runtime: {singleMovie.runtime} </h3>
+                <h3>Genre: {singleMovie.genres}</h3>
             </div>
-            <h3>{overview}</h3>
-            <Link to="/"><button>View All Movies</button></Link>
-        </div>
-    )
+                <h3>{this.state.movie.overview}</h3>
+                <Link to="/"><button>View All Movies</button></Link>
+            </div>
+        )
+    }
 }
 
 export default SingleMovie
