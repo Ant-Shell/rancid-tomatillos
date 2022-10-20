58132b62910714460351b846ca0014dc2e2c5ff4
Anthony Shellman <atshellman@gmail.com>
2022-10-20 15:35:51 -0600

    Add external API for movie details

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 const fetchAllMovieData = async () => {
     try{
-        const response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+        const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=df3fd060c14dba67f024a7e474dd71bf&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate')
         if(!response.ok) {
             console.log(response.status)
             throw new Error('An error has occurred!')
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,12 +2,14 @@ import React from "react"
 import "./Card.css"
 import {Link} from "react-router-dom"
 
-const Card = ( {title, poster, id} ) => {
+const Card = ( {title, poster, id, releaseDate, backgroundimg} ) => {
   return (
     <div className="card" id={id}>
       <Link to={`/${id}`}>
         <img className="movie-poster" src={poster} alt={`Movie poster for ${title}`}></img>
         <h2 className="movie-title">{title}</h2>
+        <p>{releaseDate}</p>
+        {/* <img className="movie-poster" src={backgroundimg} alt={`Movie poster for ${title}`}></img> */}
       </Link>
     </div>
   )
