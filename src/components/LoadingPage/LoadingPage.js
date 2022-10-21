399c713f9e8cf6904eca5643f1f08e902419af92
William Hobson <will.hobson85@gmail.com>
2022-10-20 23:00:03 -0400

    Fix link to loading page image

--- src/components/LoadingPage/LoadingPage.js
+++ src/components/LoadingPage/LoadingPage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./LoadingPage.css"
-import "../../assets/Countdown.gif"
+import countDown from "../../assets/Countdown.gif"
 
 
 const LoadingPage = () => {
     return (
       <div>
         <h1>Stay tuned for our feature presentation...</h1>
-        <img className="error-image" src="../../assets/Countdown.gif" alt="film reel countdown from 3 to 1"></img>
+        <img className="error-image" src={countDown} alt="film reel countdown from 3 to 1"></img>
       </div>
     )
 }
