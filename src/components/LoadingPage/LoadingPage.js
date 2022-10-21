1d2a458d5a33d91da5cde140937112a47c9d4827
William Hobson <will.hobson85@gmail.com>
2022-10-20 21:46:48 -0400

    Add local files to be used in error and loading screens

---
+++ src/components/LoadingPage/LoadingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./LoadingPage.css"
+import "../../assets/Countdown.gif"
+
+
+const LoadingPage = () => {
+    return (
+      <div>
+        <h1>Stay tuned for our feature presentation...</h1>
+        <img className="error-image" src="../../assets/Countdown.gif" alt="film reel countdown from 3 to 1"></img>
+      </div>
+    )
+}
+  
+  
+export default LoadingPage
