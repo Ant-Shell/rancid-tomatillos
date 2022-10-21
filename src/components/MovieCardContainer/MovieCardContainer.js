b4cc98615e273ecdd5b03ef3b548827b9cf0099f
Anthony Shellman <atshellman@gmail.com>
2022-10-21 13:57:23 -0600

    Refactor for grabbing more movies

--- src/components/MovieCardContainer/MovieCardContainer.js
+++ src/components/MovieCardContainer/MovieCardContainer.js
@@ -4,13 +4,12 @@ import Card from "../Card/Card"
 
 const MovieCardContainer = ( {movies}) => {
   const allMovies = movies.map(movie => {
-    const {id, title, poster_path, release_date, backdrop_path} = movie
+    const {id, title, poster_path, release_date} = movie
     return (
     <Card
       id={id}
       title={title}
       poster={`https://image.tmdb.org/t/p/original/${poster_path}`}
-      backgroundimg={backdrop_path}
       key={id}
       releaseDate={release_date}
     />
