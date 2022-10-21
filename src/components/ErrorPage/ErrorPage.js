42e3c60e6623d6752b3fda72885b9ce8d1556552
William Hobson <will.hobson85@gmail.com>
2022-10-20 22:59:28 -0400

    Updated error page image, styling, and imported to app

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./ErrorPage.css"
-import "../../assets/ErrorScreen.png"
+import errorImage from "../../assets/ErrorScreen.png"
 
 
 const ErrorPage = () => {
     return (
-      <div>
+      <div className="error-box">
         <h1>Oops! An error occurred while loading.</h1>
         <h2>We'll be back after we change reels!</h2>
-        <img className="error-image" src="../../assets/ErrorScreen.png" alt="two film canisters with film flowing between them in a fancy loop"></img>
+        <img className="error-image" src={errorImage} alt="two film canisters with film flowing between them in a fancy loop"></img>
       </div>
     )
 }
