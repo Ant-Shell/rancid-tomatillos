fc3a6d96359a8279817894ee38a689c433552ddf
Anthony Shellman <atshellman@gmail.com>
2022-10-22 19:12:24 -0600

    Refactor for env file usage

--- cypress/e2e/all-movie.cy.js
+++ cypress/e2e/all-movie.cy.js
@@ -1,36 +1,29 @@
-describe('all-movies', () => {
+describe('All Movies', () => {
   beforeEach(() => {
-    const API_KEY = process.env.REACT_APP_API_KEY
-    const pageNum = 1
-    cy.intercept('GET', `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNum}&with_watch_monetization_types=flatrate`, {
+    cy.intercept('GET', Cypress.env('all_movies'), {
       fixture: 'movieData.json'
     })
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:3000')
   })
   
   it('should show the title of the website', () => {
     cy.get('h1')
     .contains('A&W Movie Time')
-    
   })
   
   it('should show a collection of movies on load', () => {
     cy.get('.all-movies-container')
     .find('img')
     .should('have.length', 60)
-    
   })
 
-  it('should display movie details when clicking a movie card', () => {
+  it('should go to a different page when clicking a movie card', () => {
     cy.get('[id="718930"]').click()
     .url().should('include', '/718930')
-    .get('button').click()
   })
 
   it('should display a loading screen if no movies are loaded yet', () => {
-    const API_KEY = process.env.REACT_APP_API_KEY
-    const pageNum = 1
-    cy.intercept('GET', `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNum}&with_watch_monetization_types=flatrate`, {
+    cy.intercept('GET', Cypress.env('all_movies'), {
       fixture: 'emptyData.json'
     })
     cy.visit('http://localhost:3000')
@@ -39,9 +32,7 @@ describe('all-movies', () => {
   })
 
   it('should display an error message if a link is invalid', () => {
-    const API_KEY = process.env.REACT_APP_API_KEY
-    const id = 717728
-    cy.intercept('GET', `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`, {
+    cy.intercept('GET', Cypress.env('invalid_link'), {
       fixture: 'emptyData.json'
     })
     cy.get('[id="717728"]').click()
