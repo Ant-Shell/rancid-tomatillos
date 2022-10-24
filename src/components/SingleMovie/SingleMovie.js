aacff3e21c1a1f06992b49cec4d0b5981f98960c
William Hobson <will.hobson85@gmail.com>
2022-10-24 17:27:05 -0400

    Start writing functionality for showing trailers

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -3,6 +3,13 @@ import { fetchSpecificDetails } from '../../apiCalls'
 import ErrorPage from "../ErrorPage/ErrorPage"
 import {Link} from "react-router-dom"
 import "./SingleMovie.css"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import ReactPlayer from "react-player";
+import { Navigation, Mousewheel, Keyboard } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 class SingleMovie extends Component {
     constructor() {
@@ -10,6 +17,7 @@ class SingleMovie extends Component {
         this.state = {
             title: '',
             posterPath: '',
+            trailerSwiper: [],
             voteAverage: '',
             runTime: '',
             overview: '',
@@ -40,6 +48,21 @@ class SingleMovie extends Component {
             console.error(err)});
       }
 
+    trailers = () => {
+        let trailerArray = this.state.trailerSwiper.map((trailer) => {
+            return (
+                <SwiperSlide className="trailer-slide" key={trailer.id}>
+                  <ReactPlayer
+                    className="video"
+                    controls={true}
+                    url={`https://www.youtube.com/watch?v=${trailer.key}`}
+                  />
+                </SwiperSlide>
+              );
+        })
+        return trailerArray
+    }
+
     render = () => {
         const singleMovie = this.state
         return (
@@ -49,6 +72,20 @@ class SingleMovie extends Component {
             <div className="movie-box" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${singleMovie.backgroundImage})`}}>
                 <h2>{singleMovie.title}</h2>
                 <img className="movie-poster" src={`https://image.tmdb.org/t/p/original/${singleMovie.posterPath}`} alt={`Movie poster for ${singleMovie.title}`}></img>
+                <section className="movie-trailer">
+                    <div className="movie-trailer-view">
+                        <Swiper
+                            modules={[Navigation, Mousewheel, Keyboard]}
+                            slidesPerView={1}
+                            navigation={true}
+                            keyboard={true}
+                            mousewheel={false}
+                            className="movie-trailer-card"
+                        >
+                            {this.trailers()}
+                        </Swiper>
+                    </div>
+                </section>
                 <div className="specifics">
                 <h3 id='rating'>Rating: {parseInt(singleMovie.voteAverage).toFixed()}/10 </h3>
                 <h3 id='runtime'>Runtime: {singleMovie.runTime} minutes</h3>
