449cc8cc64317ade2ebcb8dcfd99a4aea12c5e47
Anthony Shellman <atshellman@gmail.com>
2022-10-24 09:48:08 -0600

    Pass props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,11 @@ import React from "react"
 import "./Header.css"
 import MovieSearch from "../MovieSearch/MovieSearch"
 
-const Header = ({ findMovieByTitle }) => {
+const Header = ({ findMovieByTitle, movieSearchResults, clearSearchResults }) => {
   return (
     <header>
       <h1>A&W Movie Time!</h1>
-      <MovieSearch findMovieByTitle={findMovieByTitle}/>
+      <MovieSearch findMovieByTitle={findMovieByTitle} movieSearchResults={movieSearchResults} clearSearchResults={clearSearchResults}/>
     </header>
   )
 }
