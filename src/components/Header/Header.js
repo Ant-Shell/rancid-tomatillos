76bf48cf44724dd6aa5100771201ebbbb9d126c7
Anthony Shellman <atshellman@gmail.com>
2022-10-23 18:18:57 -0600

    Add props, import statement and component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from "react"
 import "./Header.css"
+import MovieSearch from "../MovieSearch/MovieSearch"
 
-const Header = () => {
+const Header = ({ findMovieByTitle }) => {
   return (
     <header>
       <h1>A&W Movie Time!</h1>
+      <MovieSearch findMovieByTitle={findMovieByTitle}/>
     </header>
   )
 }
