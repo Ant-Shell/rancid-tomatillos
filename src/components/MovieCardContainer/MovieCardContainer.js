d0d76e26c09730c36970b5e81fd2f1524f403fae
Anthony Shellman <atshellman@gmail.com>
2022-10-23 18:19:35 -0600

    Update for search functionality

--- src/components/MovieCardContainer/MovieCardContainer.js
+++ src/components/MovieCardContainer/MovieCardContainer.js
@@ -8,8 +8,22 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-const MovieCardContainer = ( {movies}) => {
+const MovieCardContainer = ( {movies, movieSearchResults}) => {
   const allMovies = movies.map(movie => {
+    const {id, title, poster_path} = movie
+    return (
+      <SwiperSlide className="swiper-slider" key={id}>
+        <Card
+          id={id}
+          title={title}
+          poster={`https://image.tmdb.org/t/p/original/${poster_path}`}
+          key={id}
+        />
+    </SwiperSlide>
+    )
+  })
+
+  const searchedMovies = movieSearchResults.map(movie => {
     const {id, title, poster_path} = movie
     return (
       <SwiperSlide className="swiper-slider" key={id}>
@@ -34,6 +48,7 @@ const MovieCardContainer = ( {movies}) => {
           className="all-swiper-movies"
         >
         { allMovies }
+        {/* searchedMovies */}
       </Swiper>
     </div>
   )
